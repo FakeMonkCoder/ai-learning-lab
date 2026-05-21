5f85b1940d7c9a05637782588f02d1dc75940f19
RenPing <renping.sci@gmail.com>
2026-05-21 23:08:49 +0800

    update 1 learning_log.py

---
+++ practice/learning_log.py
@@ -0,0 +1,17 @@
+
+name = "Ren"
+goal = "Enter the AI field"
+current_sprint= "Sprint1"
+tools = {"Python", "Git", "GitHub", "PyCharm", "Markdown"}
+today_focus = "Git workflow and basic Python"
+
+print("Name:", name)
+print("Goal:", goal)
+print("Current Sprint:", current_sprint)
+
+print("Tools:")
+
+for tool in tools:
+    print(tool," ",end="")
+
+print("Today focus:",today_focus)
