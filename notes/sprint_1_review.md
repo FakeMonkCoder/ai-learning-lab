2a910bce40f4eda851c6e8d5b48603ae0c766008
RenPing <renping.sci@gmail.com>
2026-05-25 00:02:12 +0800

    create file counter project

---
+++ notes/sprint_1_review.md
@@ -0,0 +1,23 @@
+# sprint 1 review
+
+## 1. 这两周学会了什么
+这两周我学会了git的一系列指令
+git status git commit 等
+
+## 2. 最卡的地方是什么
+编程的函数编写 例如文件路径读取等
+
+
+## 3. Git 现在我最熟的命令有哪些
+git status  git add . git commit git push git oneline
+
+
+## 4. 对 Python 最大的新理解是什么
+python的代码原来可以使用工程的方式管理起来
+
+## 5. 现在觉得自己最弱的地方是什么
+编程能力很弱 对python语法不熟悉
+
+## 6. 下一阶段我最需要提升什么
+
+编程能力
---
+++ projects/file_counter.py
@@ -0,0 +1,34 @@
+import os
+
+def count_files(folder_path):
+
+    files = os.listdir(folder_path)
+
+
+    txtcount = 0
+    jsoncount = 0
+    mdcount = 0
+
+    for co in files:
+      if co.endswith(".txt"):
+        txtcount += 1
+      elif co.endswith(".json"):
+            jsoncount += 1
+      elif co.endswith(".md"):
+            mdcount += 1
+
+    return [txtcount,jsoncount,mdcount]
+
+
+def print_result(result):
+
+    print(".txt:",result[0])
+    print(".json:",result[1])
+    print(".md:",result[2])
+
+
+
+folder_path = "../notes"
+
+print_result(count_files(folder_path))
+
